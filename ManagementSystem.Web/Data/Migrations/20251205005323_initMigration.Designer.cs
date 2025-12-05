e43e185c2cfe9d0ef373785f82cf3276bb4e6434
jules <jules.douglas@hotmail.com>
2025-12-04 18:00:30 -0700

    Database and environment setup. An initial setup if you will

--- ManagementSystem.Web/Data/Migrations/20251205005323_initMigration.Designer.cs
+++ ManagementSystem.Web/Data/Migrations/20251205005323_initMigration.Designer.cs
@@ -4,6 +4,7 @@ using ManagementSystem.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ManagementSystem.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251205005323_initMigration")]
+    partial class initMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
